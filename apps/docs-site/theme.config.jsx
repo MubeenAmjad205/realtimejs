11bdcca279848dbe9af73db856db267ec6c8ced9
Muhammad Mubeen Amjad <157285025+MubeenAmjad205@users.noreply.github.com>
2026-06-01 21:09:58 +0500

    docs: scaffold industry-grade Nextra documentation site

---
+++ apps/docs-site/theme.config.jsx
@@ -0,0 +1,16 @@
+export default {
+  logo: <strong>RealtimeJS</strong>,
+  project: {
+    link: 'https://github.com/MubeenAmjad205/realtimejs',
+  },
+  docsRepositoryBase: 'https://github.com/MubeenAmjad205/realtimejs/tree/master/apps/docs-site',
+  footer: {
+    text: 'RealtimeJS Framework',
+  },
+  useNextSeoProps() {
+    return {
+      titleTemplate: '%s – RealtimeJS'
+    }
+  },
+  primaryHue: 210,
+}
